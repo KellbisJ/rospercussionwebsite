4cf4e68708db3c13b51241544278d726050d4cc3
KellbisJ <kellbisdevsw@gmail.com>
2025-11-08 23:00:57 -0400

    products section example

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,12 +3,15 @@ import { AboutSection } from '../../components/common/about';
 import { HeroSection } from '../../components/common/hero';
 import { ServicesSection } from '../../components/common/teaching-services';
 
+// import { ProductsSection } from '@/components/common/products-section';
+
 const HomePage = (): React.JSX.Element => {
 	return (
 		<>
 			<HeroSection />
 			<AboutSection />
 			<ServicesSection />
+			{/* <ProductsSection /> */}
 			<SocialMediaSection />
 		</>
 	);
